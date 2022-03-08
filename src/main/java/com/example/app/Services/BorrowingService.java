a8deb68c25127b6752cdc585fa3555312865f352
Lenovo <abystrican@student.umb.sk>
2022-03-08 20:26:24 +0100

    pridavam

--- src/main/java/com/example/app/Services/BorrowingService.java
+++ src/main/java/com/example/app/Services/BorrowingService.java
@@ -1,22 +1,49 @@
 package com.example.app.Services;
 
+import com.example.app.Bookdata.BookDto;
+import com.example.app.Bookdata.BookEntity;
+import com.example.app.Borrowingdata.BorrowingDto;
+import com.example.app.Borrowingdata.BorrowingEntity;
+import com.example.app.Borrowingdata.BorrowingRepository;
 import com.example.app.Objects.Borrowing;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class BorrowingService {
+    BorrowingRepository borrowingRepository;
     BookService bookService;
     CustomerService customerService;
 
-    public BorrowingService(BookService bookService, CustomerService customerService) {
+    public BorrowingService(BorrowingRepository borrowingRepository, BookService bookService, CustomerService customerService) {
+        this.borrowingRepository = borrowingRepository;
         this.bookService = bookService;
         this.customerService = customerService;
+    }
+
+    private static BorrowingDto mapToBorrowingDto(BorrowingEntity borrowingEntity) {
+        BorrowingDto borrowingDto = new BorrowingDto();
+        borrowingDto.setBookId(borrowingEntity.getBookId());
+        borrowingDto.setCustomerId(borrowingEntity.getCustomerId());
+        borrowingDto.setBook(borrowingEntity.getBook());
+        borrowingDto.setUser(borrowingEntity.getUser());
+        return borrowingDto;
+    }
+
+    @Transactional
+    public Long createBorrowing(BorrowingDto borrowingDto){
+        BorrowingEntity be = new BorrowingEntity();
+        be.setBookId(borrowingDto.getBookId());
+        be.setUser(borrowingDto.getUser());
+        be.setCustomerId(borrowingDto.getCustomerId());
+        this.borrowingRepository.save(be);
+        return be.getId();
     }
 
     /*
